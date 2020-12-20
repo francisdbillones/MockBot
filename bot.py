91cc3522340dbe0e35ae7a5c8a57ee63e1abd7d6
francisdbillones <francisd.billones@gmail.com>
2020-12-20 22:07:19 +0800

    Initial commit

--- bot.py
+++ bot.py
@@ -1,69 +1,77 @@
 import sys
 import tweepy
 import time
+import json
 
-api = authenticate(c_key, c_secret_key, a_token, a_secret_token)
+print("Authenticating...")
+
+with open("auth_assets.json", "r") as file:
+	keys = json.loads(file.read())
+	c_key = keys['APIKey']
+	c_secret_key = keys['APISecretKey']
+	a_token = keys['AccessToken']
+	a_secret_token = keys['AccessTokenSecret']
+	auth = tweepy.OAuthHandler(c_key, c_secret_key)
+	auth.set_access_token(a_token, a_secret_token)
+	api = tweepy.API(auth)
+
+print("Authentication successful.")
 
 def bot():
-	userID = input("userID to mock: ")
-	
+	userName = input("user to mock: ")
+
+	try:
+		user = api.get_user(screen_name=userName)
+	except:
+		raise Exception("The user does not exist, or the username is incorrect.")
+
+	userID = user.id
+
 	while True:
-		mock(api)
+		mock_reply(userID)
+		timer(15)
 
-def mock(api):
-	tweets = api.user_timeline(screen_name=userID, count=200, include_rts=False, tweet_mode='extended')
+def mock_reply(userID):
+	last_seen_id = retrieve_last_seen_id(userID)
 
-	for tweet in tweets:
-		if tweet.full_text in seen:
-			continue
+	print("Retrieving latest tweets...")
+	latest_tweet = api.user_timeline(user_id=userID, count=1, include_rts=False, tweet_mode='extended')[0]
+	
+	if latest_tweet.full_text == '':
+		print("Media tweet with no text to mock.")
+		return
 
-		mock_text = mock(tweet.full_text)
-		api.update_status(status=mock_text, in_reply_to_status_id=tweet.id, auto_populate_reply_metadata=True)
+	if latest_tweet.id == last_seen_id:
+		print("No new tweets yet.")
+		return
 
-		print("Mocking")
-		print("Mocked text: " + mock_text)
+	mock_text = mock(latest_tweet.full_text)
 
-		store_seen_id()
-		time.sleep(1)
+	print("Mocking...")
 
-	with open("seen.txt", "w") as saver:
-		for tweet in seen:
-			saver.write(tweet.full_text)
+	api.update_status(status=mock_text, in_reply_to_status_id=latest_tweet.id, auto_populate_reply_metadata=True)
 
-	print("Saved.")
+	print("Mocked tweet: " + latest_tweet.full_text)
+	print("Reply: " + mock_text)
 
-def store_seen_id(userID, id):
-	filename = userID + ".txt"
+	store_last_seen_id(userID, latest_tweet.id)
 
-	with open(filename, "w") as writer:
-		writer.write(id)
+def store_last_seen_id(userID, tweet_id):
+	with open("last_seen_ids.json", "r") as reader:
+		last_seen_ids = json.load(reader) 
 
-def retrieve_seen_id(userID, count):
-	ids = []
+	last_seen_ids[userID] = tweet_id
 
-	filename = userID + ".txt"
-	with open(filename, "r") as reader:
-		for i, row in enumerate(reader):
-			if i < count:
-				ids.append(row)
-			else:
-				break
-	return ids
+	with open("last_seen_ids.json", "w") as writer:
+		json.dump(last_seen_ids, writer, indent=4)
 
-
-
-
-def authenticate(c_key, c_secret_key, a_token, a_secret_token):
-	print("Authenticating...")
-	with open("auth_assets.json", "r") as file:
-		keys = json.loads(file.read())
-		c_key = keys['ConsumerKey']
-		c_secret_key = keys['ConsumerSecretKey']
-		a_token = keys['APIKey']
-		auth = tweepy.OAuthHandler(c_key, c_secret_key).set_access_token(a_token, a_secret_token)
-		api = tweepy.API(auth)
-	print("Authentication successful.")
-	return api
+def retrieve_last_seen_id(userID):
+	with open("last_seen_ids.json", "r") as file:
+		seen_ids = json.load(file)
+		if str(userID) in seen_ids:
+			return int(seen_ids[str(userID)])
+		else:
+			return None
 
 def timer(s):
 	for i in range(s, 0, -1):
@@ -71,20 +79,23 @@ def timer(s):
 		time.sleep(1)
 	print("\r")
 
-
-def read(file):
-	seen = []
-	for row in file:
-		seen.append(row)
-	return seen
-
-def mock_text(stringi):
+def getPureText(string):
+	words = string.split()
 	result = ""
-	for i, char in enumerate(string):
-		if i % 2 == 0:
-			result += char.upper()
-		else:
-			result += char.lower()
+	for i, word in enumerate(words):
+		if word[0] != '@' and word[0:4] != 'http':
+			result += word + " "
 	return result
+
+def mock(string):
+	words = getPureText(string).split()
+
+	result = ''
+	for i, word in enumerate(words):
+		mocked_text = ''
+		for j, char in enumerate(word):
+			mocked_text += (char.lower() if j % 2 == 0 else char.upper())
+		words[i] = mocked_text
+	return ' '.join(words)
 
 bot()
