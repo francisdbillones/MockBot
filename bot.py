7c79e6ab9d146ec4df16da679eba51452409595c
francisdbillones <francisd.billones@gmail.com>
2020-12-21 20:07:57 +0800

    Added logging

--- bot.py
+++ bot.py
@@ -1,10 +1,14 @@
-import sys
 import tweepy
 import time
 import json
+import logging
 
-print("Authenticating...")
+logging.basicConfig(
+	filename='info.log', filemode='w', 
+	level=logging.INFO, encoding='utf-8',
+	format='%(asctime)s: %(message)s', datefmt='%m/%d/%Y %I:%M:%S %p')
 
+logging.info("Authenticating...")
 with open("auth_assets.json", "r") as file:
 	keys = json.loads(file.read())
 	c_key = keys['APIKey']
@@ -14,8 +18,7 @@ with open("auth_assets.json", "r") as file:
 	auth = tweepy.OAuthHandler(c_key, c_secret_key)
 	auth.set_access_token(a_token, a_secret_token)
 	api = tweepy.API(auth)
-
-print("Authentication successful.")
+logging.info("Authentication successful")
 
 def bot():
 	userName = input("user to mock: ")
@@ -34,25 +37,25 @@ def bot():
 def mock_reply(userID):
 	last_seen_id = retrieve_last_seen_id(userID)
 
-	print("Retrieving latest tweets...")
+	logging.info("Retrieving latest tweets...")
 	latest_tweet = api.user_timeline(user_id=userID, count=1, include_rts=False, tweet_mode='extended')[0]
 	
 	if latest_tweet.full_text == '':
-		print("Media tweet with no text to mock.")
+		logging.info("Media tweet with no text to mock")
 		return
 
 	if latest_tweet.id == last_seen_id:
-		print("No new tweets yet.")
+		logging.info("No new tweets yet")
 		return
 
 	mock_text = mock(latest_tweet.full_text)
 
-	print("Mocking...")
+	logging.info("Mocking...")
 
 	api.update_status(status=mock_text, in_reply_to_status_id=latest_tweet.id, auto_populate_reply_metadata=True)
 
-	print("Mocked tweet: " + latest_tweet.full_text)
-	print("Reply: " + mock_text)
+	logging.info("Mocked tweet: %s" % latest_tweet.full_text)
+	logging.info("Reply: %s" % mock_text)
 
 	store_last_seen_id(userID, latest_tweet.id)
 
@@ -74,10 +77,9 @@ def retrieve_last_seen_id(userID):
 			return None
 
 def timer(s):
+	logging.info("Waiting %i seconds until next call" % s)
 	for i in range(s, 0, -1):
-		print(("\r"  + str(i) + " seconds left until next call"), end="", flush=True)
 		time.sleep(1)
-	print("\r")
 
 def getPureText(string):
 	words = string.split()
